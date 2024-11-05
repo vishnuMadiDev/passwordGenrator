843eddb37b997bdb75a139eea1abfb81b2e1499f
vishnumadireddy <vishnumadireddy@10.32.52.46>
2024-11-05 12:05:28 +0530

    first project

---
+++ src/passwordGenarator/passwordGenerator.java
@@ -0,0 +1,33 @@
+package passwordGenarator;
+import java.util.*;
+
+public class passwordGenerator {
+
+	public static void main(String[] args) {
+		
+		        int length = 6;
+		        System.out.println(Password(length));
+		   }
+		   static char[] Password(int len) {
+		        System.out.println("Generating password using random() : ");
+		        System.out.print("Your new password is : ");
+		        String Capital_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		        String Small_chars = "abcdefghijklmnopqrstuvwxyz";
+		        String numbers = "0123456789";
+		        //String symbols = "!@#$%^&*_=+-/.?<>)";
+		        String values = Capital_chars + Small_chars + numbers ;
+		        Random rndm_method = new Random();
+		        char[] password = new char[len];
+		        for (int i = 0; i < len; i++) {
+		              password[i] = values.charAt(rndm_method.nextInt(values.length()));
+		        }
+		        return password;
+		   
+			
+
+		
+
+
+	}
+
+}
